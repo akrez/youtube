049a0e471bab260d0ac2c392da4a8659c7ccf74b
akrez <akrez.like@gmail.com>
2024-04-06 14:14:38 +0330

    create ResponseService

---
+++ app/Services/ResponseService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class ResponseService implements Responsable
+{
+    public int $status = 200;
+    public string $message = 'OK';
+    public array $data = [];
+    public array $errors = [];
+
+    public function status(int $status): ResponseService
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function message(string $message): ResponseService
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function data($data): ResponseService
+    {
+        $this->data = is_array($data) ? $data : [$data];
+        return $this;
+    }
+
+    public function errors($errors): ResponseService
+    {
+        $this->errors = is_array($errors) ? $errors : [$errors];
+        return $this;
+    }
+
+    public function toResponse($request)
+    {
+        return response([
+            'message' => $this->message,
+            'data' => $this->data,
+            'errors' => $this->errors,
+        ], $this->status);
+    }
+}
