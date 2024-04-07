e30fd4f8c4ff4b823e57c3bdffc031d08fb845de
akrez <akrez.like@gmail.com>
2024-04-07 16:46:13 +0330

    SendTelegramMessageJob

---
+++ app/Jobs/SendTelegramMessageJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\TelegramHelperService;
+use App\Services\YoutubeUrlService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
+
+class SendTelegramMessageJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public $message)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $videoId = YoutubeUrlService::parse($this->message);
+        if ($videoId) {
+        }
+    }
+}
