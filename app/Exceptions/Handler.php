d6f17807796d792ed056f07af0d49e62aac22ac0
akrez <akrez.like@gmail.com>
2024-04-08 17:36:41 +0330

    some changes in Exceptions Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -31,35 +31,38 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $e)
     {
-        if ($request->expectsJson()) {
-            if ($e instanceof AuthenticationException) {
-                return $this->handle($e, $e->getMessage(), 401);
-            };
-            if ($e instanceof UnauthorizedException) {
-                return $this->handle($e, $e->getMessage(), 401);
-            };
-            if ($e instanceof NotFoundHttpException) {
-                return $this->handle($e, $e->getMessage(), 404);
-            };
-            if ($e instanceof NotFoundHttpException) {
-                return $this->handle($e, $e->getMessage(), 404);
-            };
-            if ($e instanceof ModelNotFoundException) {
-                return $this->handle($e, $e->getMessage(), 404);
-            };
-            if ($e instanceof MethodNotAllowedHttpException) {
-                return $this->handle($e, $e->getMessage(), 405);
-            };
-            if ($e instanceof ValidationException) {
-                return $this->handle($e, $e->getMessage(), 422, $e->errors());
-            };
-            if ($e instanceof ThrottleRequestsException) {
-                return $this->handle($e, $e->getMessage(), 429);
-            };
-            if ($e instanceof Throwable) {
-                return $this->handle($e, $e->getMessage(), 500);
-            };
+        if (!$request->expectsJson()) {
+            return parent::render($request, $e);
         }
+
+        if ($e instanceof AuthenticationException) {
+            return $this->handle($e, $e->getMessage(), 401);
+        };
+        if ($e instanceof UnauthorizedException) {
+            return $this->handle($e, $e->getMessage(), 401);
+        };
+        if ($e instanceof NotFoundHttpException) {
+            return $this->handle($e, $e->getMessage(), 404);
+        };
+        if ($e instanceof NotFoundHttpException) {
+            return $this->handle($e, $e->getMessage(), 404);
+        };
+        if ($e instanceof ModelNotFoundException) {
+            return $this->handle($e, $e->getMessage(), 404);
+        };
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return $this->handle($e, $e->getMessage(), 405);
+        };
+        if ($e instanceof ValidationException) {
+            return $this->handle($e, $e->getMessage(), 422, $e->errors());
+        };
+        if ($e instanceof ThrottleRequestsException) {
+            return $this->handle($e, $e->getMessage(), 429);
+        };
+        if ($e instanceof Throwable) {
+            return $this->handle($e, $e->getMessage(), 500);
+        };
+
         return parent::render($request, $e);
     }
 
