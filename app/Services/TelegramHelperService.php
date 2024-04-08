3de51b128fc37c86906dd442cc2c2e6f2a055319
Akrez <akrez.like@gmail.com>
2024-04-08 04:53:33 +0330

    sendMessage

--- app/Services/TelegramHelperService.php
+++ app/Services/TelegramHelperService.php
@@ -7,6 +7,26 @@ use Illuminate\Support\Facades\Http;
 
 class TelegramHelperService
 {
+    public static function sendMessage($chatId, $text, $replyToMessageId)
+    {
+        $token = env('TELEGRAM_BOT_TOKEN');
+
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $text,
+            'reply_to_message_id' => $replyToMessageId,
+        ];
+
+        $url = "https://api.telegram.org/bot$token/sendMessage";
+        $headers = [];
+        if (1) {
+            $headers['X-POWERED-BY'] = $url;
+            $url = "https://agent.aliakbarrezaei.ir/";
+        }
+
+        return Http::withHeaders($headers)->post($url, $data);
+    }
+
     public static function getUpdates($offset = null, $limit = 200)
     {
         $token = env('TELEGRAM_BOT_TOKEN');
@@ -16,14 +36,14 @@ class TelegramHelperService
             'offset' => $offset,
         ];
 
-        $url = "https://api.telegram.org/bot$token/getUpdates" . "?" . http_build_query($data);
+        $url = "https://api.telegram.org/bot$token/getUpdates";
         $headers = [];
         if (1) {
             $headers['X-POWERED-BY'] = $url;
             $url = "https://agent.aliakbarrezaei.ir/";
         }
 
-        return Http::withHeaders($headers)->get($url);
+        return Http::withHeaders($headers)->get($url, $data);
     }
 
     public static function createTelegramUpdate($status, $response, $offset)
