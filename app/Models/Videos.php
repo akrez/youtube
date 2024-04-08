64efca2e391833c5a219e28c52eb160d070db4da
akrez <akrez.like@gmail.com>
2024-04-08 17:37:40 +0330

    some changes in Videos Model

--- app/Models/Videos.php
+++ app/Models/Videos.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Videos extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $casts = [
+        'synced_at' => 'datetime',
+        'formats' => 'json',
+        'adaptive_formats' => 'json',
+    ];
+
+    public function scopeFilterRecently(Builder $query): void
+    {
+        $query->where('synced_at', '>=', now()->subHours(1)->toDateTimeString());
+    }
 }
